75730a9c35df831cfebf0454f269cedbbce0e69e
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-01-27 22:32:52 -0300

    initial nova estrutura

--- wallet/src/test/java/com/devleandro/wallet/DemoApplicationTests.java
+++ wallet/src/test/java/com/devleandro/wallet/DemoApplicationTests.java
@@ -1,15 +1,13 @@
-package com.wallet;
+package com.devleandro.wallet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class DemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
-
+	void contextLoads() {
 	}
+
 }
